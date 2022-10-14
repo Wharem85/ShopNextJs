c988d3f6339d3ed3bf72e0229ca85ce8e65ead1f
Wharem85-25 <wharem85@hotmail.com>
2022-10-14 15:13:33 -0600

    se configuro el next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   images: {
-    domains: ['api.lorem.space', 'placeimg.com', 'wixmp.com', 'romapy'],
+    domains: ['api.lorem.space', 'placeimg.com'],
   },
 };
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  include: ['production'],
+  register: true,
+});
+
+module.exports = withPWA(nextConfig);
